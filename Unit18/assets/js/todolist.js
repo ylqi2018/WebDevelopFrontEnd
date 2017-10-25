bf9b1ae1cde5e76a035bbcdbeae8f7935f01f095
ylqi007 <yunlongqi@my.unt.edu>
2017-10-25 13:23:24 -0500

    Unit 18, check out specific item

--- Unit18/assets/js/todolist.js
+++ Unit18/assets/js/todolist.js
@@ -1,1 +1,26 @@
-// alert("co")
+// check off specific todo by click
+// $("li").click(function() {
+// 	// $(this).css("color", "gray");
+// 	// $(this).css("text-decoration", "line-through");
+	
+// 	console.log($(this).css("color"));
+// 	// if li is gray
+// 	if($(this).css("color") === "rgb(128, 128, 128)") {
+// 		// turn it back
+// 		// console.log("This is currently gray");
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+		
+// 	} else {
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// })
+
+$("li").click(function() {
+	$(this).toggleClass("completed");
+})
